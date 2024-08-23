c550279d38bc812ea7fabd654ea38f376f101b16
b41r4m <balramas11@gmail.com>
2024-08-23 22:31:25 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactsData from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+
+  let contactList = contacts.map((contact) => (
+    <tr key={contact.id} className="single-contact">
+      <td>
+        <img src={contact.pictureUrl} alt={contact.name} />
+      </td>
+      <td>{contact.name}</td>
+      <td>{contact.popularity.toFixed(2)}</td>
+      <td>{contact.wonOscar ? "🏆" : "-"}</td>
+      <td>{contact.wonEmmy ? "🏆" : "-"}</td>
+      <td id={contact.id}>
+        <button onClick={contactDelete} className="del-button">
+          Delete
+        </button>
+      </td>
+    </tr>
+  ));
+
+  let addRandomContact = () => {
+    let contactsID = contacts.map((contact) => contact.id);
+    let otherContacts = contactsData.filter(
+      (contact) => !contactsID.includes(contact.id)
+    );
+    let randomIndex = Math.floor(Math.random() * otherContacts.length);
+    setContacts([...contacts, otherContacts[randomIndex]]);
+  };
+
+  let sortByPopularity = () => {
+    let contactList = [...contacts];
+    contactList.sort((a, b) => b.popularity - a.popularity);
+    setContacts(contactList);
+  };
+
+  let sortByName = () => {
+    let contactList = [...contacts];
+    contactList.sort((a, b) => {
+      if (b.name > a.name) return -1;
+      if (b.name < a.name) return 1;
+      return 0;
+    });
+    setContacts(contactList);
+  };
+
+  function contactDelete(e) {
+    let cID = e.target.parentNode.id;
+    let contactList = contacts.filter((contact) => contact.id !== cID);
+    setContacts(contactList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>🌟CONTACTS🌟</h1>
+      <div className="buttons">
+        <button onClick={addRandomContact}>Add Random Contact</button>
+        <button onClick={sortByPopularity}>Sort by popularity</button>
+        <button onClick={sortByName}>Sort by name</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won-Oscar</th>
+            <th>Won-Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{contactList}</tbody>
+      </table>
     </div>
   );
 }
